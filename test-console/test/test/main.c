336c8be34976e264399a65921dad8ca3eaf40258
Shane Harris <shanejh@Kenny.local>
2011-05-18 09:07:05 +1200

    sdfsfsfsf

---
+++ test-console/test/test/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  test
+//
+//  Created by Shane Harris on 18/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Lets add another line");
+    return 0;
+}
+
